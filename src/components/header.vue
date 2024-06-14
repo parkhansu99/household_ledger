22b59c3a897422f95d6f4c4b93ba15ff87aada2b
Hansu <0526hans@gmail.com>
2024-06-14 09:49:32 +0900

    6/14 9:49

---
+++ src/components/header.vue
@@ -0,0 +1,49 @@
+<template>
+    <nav class="navbar navbar-expand-sm bg-light navbar-light">
+        <span class="navbar-brand ps-2">HOUSEHOLD</span>
+        <button
+            class="navbar-toggler"
+            type="button"
+            @click="isNavShow = !isNavShow"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+            :class="
+                isNavShow
+                    ? 'collapse navbar-collapse show'
+                    : 'collapse navbar-collapse'
+            "
+            class="justify-content-between"
+        >
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/">Calender</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/inout"
+                        >InOut</router-link
+                    >
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/report"
+                        >Report</router-link
+                    >
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/profile"
+                        >Profile</router-link
+                    >
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const isNavShow = ref(false);
+</script>
---
+++ src/pages/inOut.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row">
+    <div class="col p-3">
+      <ul class="list-group">
+        <inoutItem />
+      </ul>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col p-3">
+      <router-link class="btn btn-secondary" to="/inout/add">
+        내역 추가
+      </router-link>
+      <button class="btn btn-secondary ms-1" @click="fetchInoutList">
+        새로 고침
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useInoutListStore } from "../stores/inoutList.js";
+import inoutItem from "../pages/inoutItem.vue";
+const inoutListStore = useInoutListStore();
+const { fetchInoutList } = inoutListStore;
+const inoutList = computed(() => inoutListStore.inoutList);
+</script>
